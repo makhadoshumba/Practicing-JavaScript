0dfc4246d7b8cbc3c76b62f4462c6d1d9ff51bdd
Shumba Makhado <makhadoshumba@gmail.com>
2026-07-22 23:24:28 +0200

    day 3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function lighton() {
   document.getElementById("img").src = "lighton.jpg";
-  window.alert("The light is on");
+  alert("The light is on");
 }
 
 function lightoff() {
   document.getElementById("img").src = "lightoff.jpg";
-  window.alert("The light is off");
+  alert("The light is off");
 }
